6a4dc7f7478044f93562cd60759931f44450d555
unknown <yadavindal269@gmail.com>
2020-12-31 08:26:46 +0530

    initial commit

--- day12/flutterapp/lib/main.dart
+++ day12/flutterapp/lib/main.dart
@@ -10,8 +10,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: Text("indal"),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      home: HomeScreen(),
+      
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      drawer: Drawer(
+        child: Text("hello"),
+      ),
+      appBar: AppBar(
+        title: Text("My Awesome App"),
+        backgroundColor: Colors.black87,
+      ),
+      body: Container(
+        child: Center(
+          child: Text("indal kumar yadav"),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.message_sharp),
+            title: Text("Messages"),
+          ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Profile"),
+          ),
+          
+        ],
+
+      ),
       
     );
   }
