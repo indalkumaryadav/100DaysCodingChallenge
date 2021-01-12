7af49b51bb911b545ef4ab2da4cbd759ca9efe45
Indal Yadav <yadavindal269@gmail.com>
2021-01-12 11:06:52 +0530

    initial commit

--- day24/django_ajax/apps/core/views.py
+++ day24/django_ajax/apps/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import ImageForm
 
 # Create your views here.
+def home_view(request):
+    form=ImageForm
+    context={
+        'form':form
+    }
+    return render(request,"core/home.html",context)
