cf3fa66ae9f4f6a383e1fb2ef8983e37419bc01c
Indal Yadav <yadavindal269@gmail.com>
2021-01-17 17:51:10 +0530

    initial commit

--- day29/mysite/js/main.js
+++ day29/mysite/js/main.js
@@ -1,1 +1,10 @@
-console.log("hello World.");
+// console.log("hello World.");
+
+// const reader = new FileReader();
+// const indal = document.querySelector("input");
+
+// indal.addEventListener("change", () => {
+//   reader.onload("onload", () => {
+//     console.log(reader);
+//   });
+// });
