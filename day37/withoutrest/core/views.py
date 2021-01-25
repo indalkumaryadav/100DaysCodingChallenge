7fe9aa10a94bc747759cf4812e995b2a50efdead
Indal Yadav <yadavindal269@gmail.com>
2021-01-25 17:32:13 +0530

    first commit

--- day37/withoutrest/core/views.py
+++ day37/withoutrest/core/views.py
@@ -5,18 +5,24 @@ from django.views.generic import View
 from .forms import CreatePostForm
 from .models import Post
 import json
+from django.core.serializers import serialize
+from django.http import JsonResponse
 
 class JsonData(View):
 
     def get(self,request):
         post=Post.objects.all()
-        return HttpResponse(content_type="application/json",status=200)
+        json_data=serialize('json',[post,])
+        print(json_data)
+        return JsonResponse(json_data)
 
     def post(self,request):
+        data=request.body
+        valid_json=is_json(data)
         form=CreatePostForm()
-
         if form.is_valid():
             form.save()
+            json_data=json.dumps({'msg':"Successfully Created!"})
         else:
             json_data=json.dumps(form.errors)
         return HttpResponse(content_type="application/json")
