a700b6696754baffda98a1c96ba1138cade6364f
Indal Kumar <yadavindal269@gmail.com>
2021-02-01 17:57:19 -0800

    first commit

---
+++ day45/reactapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Indal</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
