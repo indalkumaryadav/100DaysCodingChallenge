6a2d964a614e96a0c1caf6a0654ce59e282fc690
indalkumaryadav <yadavindal269@gmail.com>
2021-02-04 18:23:19 +0530

    initial commit

---
+++ day47/my-app/src/component/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <h1>Hello</h1>
+        </div>
+        <div className="col-3">
+          <h1>Hello</h1>
+        </div>
+        <div className="col-3">
+          <h1>Hello</h1>
+        </div>
+        <div className="col-3">
+          <h1>Hello</h1>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default Home;
