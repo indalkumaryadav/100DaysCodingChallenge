da4094989cfdf3483a1b3a67fb4c43456d20a994
indalkumaryadav <yadavindal269@gmail.com>
2021-02-07 05:11:55 +0530

    first commit

--- day50/myproject/core/admin.py
+++ day50/myproject/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Student,Student1,Teacher,Teacher1
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Student1)
+admin.site.register(Teacher)
+admin.site.register(Teacher1)
--- day50/myproject/core/models.py
+++ day50/myproject/core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+# Abstract base model in heritance
+
+class ModelInfo(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    class Meta:
+        abstract=True
+
+class Student(ModelInfo):
+    roll=models.IntegerField()
+    subject=models.CharField(max_length=100)
+
+class Teacher(ModelInfo):
+    teaching_subject=models.CharField(max_length=100)
+    salery=models.IntegerField()
+
+
+# Multiple inheritance
+
+class ModelInfo1(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+  
+class Student1(ModelInfo):
+    roll=models.IntegerField()
+    subject=models.CharField(max_length=100)
+
+class Teacher1(ModelInfo):
+    teaching_subject=models.CharField(max_length=100)
+    salery=models.IntegerField()
+
+# MiltiLavel Inheritance
+
+class Parent(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+  
+class Child(Parent):
+    roll=models.IntegerField()
+    subject=models.CharField(max_length=100)
+
+class SubChild(Child):
+    teaching_subject=models.CharField(max_length=100)
+    salery=models.IntegerField()
