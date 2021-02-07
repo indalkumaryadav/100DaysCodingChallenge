c46584753325f6edc6ecfd14e0b7b3cebbe22224
indalkumaryadav <yadavindal269@gmail.com>
2021-02-07 06:35:54 +0530

    initial commit

--- day50/myproject/core/models.py
+++ day50/myproject/core/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Abstract base model in heritance
 
+class Manager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by("-id")
+    
+    def name_age(self,name,age):
+        return super().get_queryset().filter(name=name,age=age)
+    
+    def std_shoted_by(self,param):
+        return super().get_queryset().order_by(param)
+    
+    # stde=Student.objects.name_age()
+    # stde=Student.objects.std_shoted_by("name")
+
 class ModelInfo(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
@@ -11,6 +24,7 @@ class ModelInfo(models.Model):
 class Student(ModelInfo):
     roll=models.IntegerField()
     subject=models.CharField(max_length=100)
+    my_objects=Manager()
 
 class Teacher(ModelInfo):
     teaching_subject=models.CharField(max_length=100)
@@ -44,3 +58,10 @@ class Child(Parent):
 class SubChild(Child):
     teaching_subject=models.CharField(max_length=100)
     salery=models.IntegerField()
+
+
+
+# Proxy Model Inheritance
+# class ProxyModel(Student):
+#     class Meta:
+#         proxy=True
