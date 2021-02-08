db260f6f01bee788a25093f03d9858e70e4d8433
indalkumaryadav <yadavindal269@gmail.com>
2021-02-08 17:00:59 +0530

    first commit

---
+++ day51/flaskapp/main.py
@@ -0,0 +1,13 @@
+import flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app=flask.Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template ("index.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
