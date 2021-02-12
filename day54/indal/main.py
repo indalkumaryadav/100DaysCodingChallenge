78a904d8d7e35bee70404461f63787f556e4f786
indalkumaryadav <yadavindal269@gmail.com>
2021-02-12 07:02:21 +0530

    first commit

--- day54/indal/main.py
+++ day54/indal/main.py
@@ -1,2 +1,9 @@
 indal="indal kumar yadav"
-print(indal)
+# ** power opeartor
+# print(5.0//2)
+# print(5%2)
+# print(5**2)
+# print('durga'+4)error
+print('durga'*4)
+print('durga'*4.0)
+# print('durga'*'4') error
