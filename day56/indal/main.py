2394b29ab712a665063736fd14bfbd6d21f992c3
indalkumaryadav <yadavindal269@gmail.com>
2021-02-13 16:11:09 +0530

    first commit

--- day56/indal/main.py
+++ day56/indal/main.py
@@ -2,4 +2,45 @@
 
 from sys import argv
 
-print(argv[1:])
+# sum=0
+# for x in argv[1:]:
+#     sum=sum+int(x)
+# print(sum)
+
+kumar="kumar"
+name=f"indal {kumar}"
+f="hello my name  is {1} kumar and age is {0}".format("indal",20)
+
+# n=int(input("enter number \n"))
+# sum=0
+# i=1
+# while i<=n:
+#     sum=sum+i
+#     i=i+1
+
+# nested Loop python
+# for i in range(10):
+#     for j in range(10):
+#         print(i,j)
+
+
+# rows=int(input("Enter number of rows:- \n"))
+
+# for i in range(1,rows+1):
+#     for j in range(1,rows+1):
+#         print("*",end='')
+#     print()
+
+# d={
+#     'name':'indal kumar',
+#     'age':20
+# }
+# for i,j in d.items():
+#     print(i,j)
+
+
+l=[1,2,5,3,5]
+l2=[10,10]
+# l2.extend(l)
+# print(l2)
+# print(l2.remove(10))
