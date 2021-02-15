1f6b90886baa4c582cbde495ddefca86c4e254fa
indalkumaryadav <yadavindal269@gmail.com>
2021-02-15 08:24:49 +0530

    first commit of day 58

--- day57/reactapp/src/App.js
+++ day57/reactapp/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PaymentRazorpay from "./components/PaymentRazorpay";
+import { Switch, Route } from "react-router-dom";
+import About from "./components/About";
 function App() {
   return (
     <>
-      <PaymentRazorpay />
+      <Switch>
+        <Route exact path="/" component={PaymentRazorpay} />
+        <Route exact path="/about" component={About} />
+      </Switch>
     </>
   );
 }
