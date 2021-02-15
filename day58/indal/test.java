d5a98457503d5122bb07e8f27ae64bf02bdbb2fa
indalkumaryadav <yadavindal269@gmail.com>
2021-02-15 16:59:59 +0530

    first commit

---
+++ day58/indal/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        byte n=10;
+        System.out.println(n);
+    }
+}
+class test2{
+    public static void main(String[] args){
+        int n=10;
+        System.out.println(n);
+    }
+}
---
+++ day58/reactapp/src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <button className="btn btn-primary">Add User</button>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
