c3426b2cf6f8086a506b420041e96e7242d07db8
indalkumaryadav <yadavindal269@gmail.com>
2021-02-17 10:03:08 +0530

    first commit

---
+++ day60/indal/main.py
@@ -0,0 +1,8 @@
+# x=10
+
+# assert x==10, "Here is 10 is equal to 10"
+
+# assert x>10
+# print(x)
+f=open("indal.txt", "w")
+print(f.write("er"))
---
+++ day60/indal/test.py
@@ -0,0 +1,8 @@
+def square(x):
+    return x**x
+
+assert square(10)==100, 'the square of 10 should be 100'
+assert square(2)==4, 'the square of 2 should be 4'
+print(square(2))
+print(square(3))
+print(square(4))
