6e150841b98cc2d644925a972162776de6798311
indalkumaryadav <yadavindal269@gmail.com>
2021-02-20 07:17:21 +0530

    first commit

--- day63/indal/main.py
+++ day63/indal/main.py
@@ -1,6 +1,19 @@
 class Test:
-    @staticmethod
-    def m1(cls):
-        print(cls)
 
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+    def display(self):
+        print(self.name)
+        print(self.age)
+    
 
+# class Employee:
+
+    # def modify(emp):
+    #     emp.age=emp.age+5
+    #     emp.display()
+
+# t=Test("Indal kumar",20)
+# e=Employee.modify(t)
