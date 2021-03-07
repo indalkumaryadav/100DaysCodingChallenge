6512fec153702ebf9b67170ecadb79df46edfc36
indalkumaryadav <yadavindal269@gmail.com>
2021-03-07 11:32:24 +0530

    first commit

---
+++ day78/indal/indal.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print("indal Kumar Hello World");
+}
+class Student{
+  
+  void marks(){
+    print("Your marks is 100");
+  }
+}
--- day78/my-app/src/App.js
+++ day78/my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World.</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit fugit quam
+        hic, delectus adipisci corrupti quae, quia amet maiores ab porro aliquid
+        dolore voluptate sapiente exercitationem sequi explicabo! Eligendi,
+        minus?
+      </p>
+      <h2>Indal Kumar Yadav</h2>
     </div>
   );
 }
